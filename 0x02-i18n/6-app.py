988c3f7eb5e58bd801f752895a95872cb407bd70
Mythamor <bethmithamo.bm@gmail.com>
2024-01-10 01:24:34 +0300

    get_locale function to use a user’s preferred local if it is supported

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+"""
+Module: 5-app
+"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from datetime import datetime, timezone
+
+
+class Config:
+    """
+    Set up app defaults
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Coinfigure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """
+    Get user from the dict above
+    """
+    logged_in = request.args.get('login_as')
+    if logged_in:
+        return users.get(int(logged_in))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """
+    To be executed before all other functions
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get locale from  user’s preferred local if it is supported
+    priority:
+            Locale from URL parameters
+            Locale from user settings
+            Locale from request header
+            Default locale
+    """
+    # Locale from url params
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+
+    # Locale from user settings
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
+
+    # Locale from request header
+    locale = request.headers.get('locale', None)
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    # Default locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index():
+    """
+    Home page view
+    """
+    return render_template('6-index.html', title='Welcome to Holberton')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
