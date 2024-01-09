84e7c7c866288ed694e6faa70e8f6e521d4dd0f8
Mythamor <bethmithamo.bm@gmail.com>
2024-01-09 15:27:23 +0300

    Flask i18n tasks

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -17,5 +17,5 @@ def index():
     return render_template('0-index.html', title='Welcome to Holberton')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
