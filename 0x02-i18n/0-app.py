0f622118df7ae23a4bdb16bcc455834e5914bf90
Mythamor <bethmithamo.bm@gmail.com>
2024-01-09 15:11:38 +0300

    Flask i18n tasks

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,14 +4,17 @@
 Module: 0-app
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from flask import render_template
-
 
 @app.route('/')
-@app.route('/index')
 def index():
+    """
+    Home page view
+    """
     return render_template('0-index.html', title='Welcome to Holberton')
+
+if __name__ == "__main__":
+    app.run(debug=True)
